416a7b04507bc3968c70a2350c6463e50228d471
Adrian E <addis.aden@gmail.com>
2013-11-24 17:06:29 +0100

    init

---
+++ test/renoise_osc_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns renoise-osc-clj.core-test
+  (:require [clojure.test :refer :all]
+            [renoise-osc-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
