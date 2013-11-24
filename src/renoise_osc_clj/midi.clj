aeac3dea37b9fa5b44e08a011c66feeb03531af2
Adrian E <addis.aden@gmail.com>
2013-11-25 00:54:45 +0100

    create sym->midi

---
+++ src/renoise_osc_clj/midi.clj
@@ -0,0 +1,24 @@
+(ns renoise-osc-clj.midi)
+
+(def notes- {\c 0
+             \d 2
+             \e 4
+             \f 5
+             \g 7
+             \a 9
+             \b 11
+             })
+
+(def signs- {\_ -1
+             \# 1
+             })
+
+(def octaves- {\1 2 \2 3 \3 4 \4 5 \5 6 \6 7 \7 8 \8 9})
+
+(defn sym->midi [input]
+  (let [inn (seq (name input))
+        nnil (fn [i] (some #(when (not (nil? %)) %) i))
+        n (nnil (map notes- inn))
+        s (nnil (map signs- inn))
+        o (nnil (map octaves- inn))]
+    (+ (+ (if n n 0) (if s s 0)) (* (if o o 1) 12))))
