790bc5c7f858c63cf2e509cbfde8b7be7b944b95
Adrian E <addis.aden@gmail.com>
2013-11-25 01:07:34 +0100

    sym->midi can have now multiple args

--- src/renoise_osc_clj/midi.clj
+++ src/renoise_osc_clj/midi.clj
@@ -15,10 +15,12 @@
 
 (def octaves- {\1 2 \2 3 \3 4 \4 5 \5 6 \6 7 \7 8 \8 9})
 
-(defn sym->midi [input]
-  (let [inn (seq (name input))
-        nnil (fn [i] (some #(when (not (nil? %)) %) i))
-        n (nnil (map notes- inn))
-        s (nnil (map signs- inn))
-        o (nnil (map octaves- inn))]
-    (+ (+ (if n n 0) (if s s 0)) (* (if o o 1) 12))))
+(defn sym->midi
+  ([input]
+    (let [inn (seq (name input))
+          nnil (fn [i] (some #(when (not (nil? %)) %) i))
+          n (nnil (map notes- inn))
+          s (nnil (map signs- inn))
+          o (nnil (map octaves- inn))]
+      (+ (+ (if n n 0) (if s s 0)) (* (if o o 1) 12))))
+  ([inn & args] (map sym->midi (conj args inn))))
